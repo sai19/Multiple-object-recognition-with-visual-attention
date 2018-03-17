803c6210cc690eb20bc80881cc61b8c867044924
Sai <s.koturwar@gmail.com>
2018-03-17 16:11:17 +0900

    added train file

--- train.py
+++ train.py
@@ -9,9 +9,23 @@ import numpy as np
 import cv2
 from core import layer
 
-M = 10 # monte carlo samples
-resized_input = Input(shape=(16,16,3))
-image_input = Input(shape=(28,28,3))
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train = x_train/255
+#x_train[x_train>0.5] = 1
+#x_train[x_train<=0.5] = 0
+#x_train = x_train.reshape(x_train.shape[0], 28*28)
+
+M = 2 # monte carlo samples
+nb_classes = 10;
+y_train = keras.utils.to_categorical(y_train, nb_classes)
+y_test = keras.utils.to_categorical(y_test, nb_classes)
+resized_input = Input(shape=(16,16,1))
+x_resized = np.resize(x_train,(x_train.shape[0],16,16))
+x_resized = np.reshape(x_resized,(x_resized.shape[0],16,16,1))
+x_train = np.reshape(x_train,(x_train.shape[0],28,28,1))
+image_input = Input(shape=(28,28,1))
+pred_input = Input(shape=(nb_classes,))
 
 ## build context network
 context_param = {}
@@ -46,7 +60,7 @@ glimpse_net.build_layers()
 ## build classification network
 class_param = {}
 class_param["class_hidden_size"] = 128
-class_param["nb_classes"] = 2
+class_param["nb_classes"] = nb_classes
 class_net = layer.class_net(**class_param)
 class_net.build_layers()
 
@@ -57,10 +71,16 @@ for i in range(M):
 		e = emission_net.get_emission_out(r2)
 		g = glimpse_net.get_glimpse_out(image_input,e)
 		out = class_net.get_classification_out(r1)
+		loss = K.categorical_crossentropy(pred_input,out)
 	else:
 		r1,r2 = recurrent_net.get_recurrent_out(g)
 		e = emission_net.get_emission_out(r2)
 		g = glimpse_net.get_glimpse_out(image_input,e)
 		out = class_net.get_classification_out(r1)
-model = Model([resized_input,image_input],[out])
-model.summary()			 
+		loss = loss + K.categorical_crossentropy(pred_input,out)
+loss = loss/M		
+model = Model([resized_input,image_input,pred_input],[out])
+model.add_loss(loss)
+model.compile(optimizer="rmsprop")
+model.summary()
+model.fit([x_resized,x_train,y_train],verbose=1,epochs=10)			 
